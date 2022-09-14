1d7bd31459af836402343632517f1b48a554d46b
sashant <sashant630@gmail.com>
2022-09-14 12:50:02 +0530

    added main content using Reactmarkdown

--- material-ui-blog/src/components/Main.js
+++ material-ui-blog/src/components/Main.js
@@ -1,20 +1,86 @@
 import { Divider, Grid, Typography } from '@mui/material'
 import React from 'react'
-// import Markdown from "markdown-to-jsx"
+import ReactMarkdown from 'react-markdown'
 // import { posts } from '../data/Data'
 
-const Main = ({title}) => {
+const Main = ({title}) => { 
+
+  const markdown = `# Sample blog post
+
+  #### April 1, 2020 by [Olivier](/)
+  
+  This blog post shows a few different types of content that are supported and styled with
+  Material styles. Basic typography, images, and code are all supported.
+  You can extend these by modifying Markdown.js.
+  
+  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+  Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.
+  Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.
+  
+  Curabitur blandit tempus porttitor. **Nullam quis risus eget urna mollis** ornare vel eu leo.
+  Nullam id dolor id nibh ultricies vehicula ut id elit.
+  
+  Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum.
+  Aenean lacinia bibendum nulla sed consectetur.
+  
+  ## Heading
+  Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+  Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+  Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+  
+  ### Sub-heading
+  
+  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+  
+  ### Sub-heading
+  
+  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+  Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod.
+  Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo
+  sit amet risus.
+  
+  - Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+  - Donec id elit non mi porta gravida at eget metus.
+  - Nulla vitae elit libero, a pharetra augue.
+  Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.
+  1.  Vestibulum id ligula porta felis euismod semper.
+  2.  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+  3.  Maecenas sed diam eget risus varius blandit sit amet non magna.
+  Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.
+  # Another blog post
+  #### March 23, 2020 by [Matt](/)
+  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+  Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.
+  Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.
+  Curabitur blandit tempus porttitor. **Nullam quis risus eget urna mollis** ornare vel eu leo.
+  Nullam id dolor id nibh ultricies vehicula ut id elit.
+  Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum.
+  Aenean lacinia bibendum nulla sed consectetur.
+  Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+  Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+  Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+  # New feature
+  #### March 14, 2020 by [Tom](/)
+  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+  Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod.
+  Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh,
+  ut fermentum massa justo sit amet risus.
+  - Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+  - Donec id elit non mi porta gravida at eget metus.
+  - Nulla vitae elit libero, a pharetra augue.
+  Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum.
+  Aenean lacinia bibendum nulla sed consectetur.
+  Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.`
+
+ 
+
   return (
     <Grid item xs={12} md={8}>
       <Typography variant='h6' gutterBottom>
         {title}
       </Typography>
       <Divider />
-      {/* {
-        posts.map(post => (
-          {/* <Markdown key={post.body}> {post.body}</Markdown> */}
-        ))
-      } */}
+          <ReactMarkdown children={markdown}/>
     </Grid>
   )
 }
